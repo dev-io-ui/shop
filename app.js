86f282e5afa59ca3eb6916a5caa8554f03fb2abc
Akash <akash.puranik06@gmail.com>
2024-11-26 03:03:51 +0530

    relations , belongsTo , hasMany , user and product

--- app.js
+++ app.js
@@ -15,17 +15,46 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then((user)=>{
+        req.user=user;
+        next();
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User ,{constraints : true ,onDelete:'CASCADE'});
+User.hasMany(Product);
+
+
 sequelize.sync()
+.then((result)=>{
+    return User.findByPk(1);
+})
+.then((user)=>{
+    if(!user)
+    {
+        return User.create({name:'dev',email:'dev@gamil.com'});
+    }
+    return user
+})
 .then((res)=>{
     app.listen(4001);
     // console.log(res);
